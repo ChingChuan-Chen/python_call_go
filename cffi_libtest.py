4d594caac1b9247e719c3edc335e50eceb4d3d00
Jamal Chen <zw12356@gmail.com>
2018-04-24 22:51:21 +0800

    modify the GoInt type

--- cffi_libtest.py
+++ cffi_libtest.py
@@ -1,17 +1,11 @@
 import os
 os.system("go build -buildmode=c-shared -o libtest.dll test.go")
 
-import sys
 from cffi import FFI
-is_64b = sys.maxsize > 2**32
 
 ffi = FFI()
-if is_64b:
-    ffi.cdef("typedef long GoInt;\n")
-else:
-    ffi.cdef("typedef int GoInt;\n")
-
 ffi.cdef("""
+typedef long long GoInt;
 void printBye();
 GoInt sum(GoInt p0, GoInt p1);
 """)
